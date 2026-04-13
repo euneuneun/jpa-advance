4c77ff8284383e99dcd09e060ea421ff240bce15
euneuneun <daeun2597@gmail.com>
2026-04-14 00:46:39 +0900

    @ManyToOne 단방향 테스트

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -13,13 +13,5 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    @OneToOne(mappedBy = "user")
-    private Food food;
-
-    public void addFood(Food food) {
-        this.food = food;
-        food.setUser(this);
-    }
 }
 
